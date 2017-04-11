78c300bb4b0d860c63ef0486b804ada97929f5ea
schneems <richard.schneeman@gmail.com>
2017-04-11 18:27:59 -0500

    Failing test
    When system ruby (1.9.3) is used a gemspec containing 
    
    
    ```
      if spec.respond_to?(:metadata)
        spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
      else
        raise "RubyGems 2.0 or newer is required to protect against " \
          "public gem pushes."
      end
    ```
    
    Cannot be installed.
    
    The buildpack vendors in a custom version of ruby (that matches the default version of ruby if non is specified in the Gemfile. However when there are bugs and system ruby is used instead then this will fail.

--- spec/hatchet/ci_spec.rb
+++ spec/hatchet/ci_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
 
 describe "CI" do
-  it "Does not cause the double ruby rainbow bug" do
-    Hatchet::Runner.new("heroku-ci-json-example").run_ci do |test_run|
+  # it "Does not cause the double ruby rainbow bug" do
+  #   Hatchet::Runner.new("heroku-ci-json-example").run_ci do |test_run|
+  #     expect(test_run.status).to eq(:succeeded)
+  #   end
+  # end
+
+  it "Fails if system ruby (1.9.3) is used and gem has a raise in the gemspec" do
+    Hatchet::Runner.new("bad_gemspec_ci").run_ci do |test_run|
       expect(test_run.status).to eq(:succeeded)
     end
   end

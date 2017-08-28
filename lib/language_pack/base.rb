e6b612696aef20116f7fa73a7b14125addcbd544
schneems <richard.schneeman@gmail.com>
2017-08-28 15:18:11 -0500

    staging-bucket

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -16,7 +16,7 @@ class LanguagePack::Base
   include LanguagePack::ShellHelpers
   extend LanguagePack::ShellHelpers
 
-  VENDOR_URL           = ENV['BUILDPACK_VENDOR_URL'] || "https://s3-external-1.amazonaws.com/heroku-buildpack-ruby"
+  VENDOR_URL           = ENV['BUILDPACK_VENDOR_URL'] || "https://s3-external-1.amazonaws.com/heroku-buildpack-ruby-staging"
   DEFAULT_LEGACY_STACK = "cedar"
   ROOT_DIR             = File.expand_path("../../..", __FILE__)
 
